5726ac3e1eb81e5189ada581535537531ffd5ec1
adachi-shu <backnumber0620@icloud.com>
2023-02-08 05:05:30 +0900

    edit node.js.yml

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -9,12 +9,12 @@ export default function Avatar({ author }) {
   return (
     <div className="flex items-center">
       <div className="w-12 h-12 relative mr-4">
-        <Image
+        {/* <Image
           src={author.node.avatar.url}
           layout="fill"
           className="rounded-full"
           alt={name}
-        />
+        /> */}
       </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
